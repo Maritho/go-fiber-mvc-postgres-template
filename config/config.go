746a8e216fcf5b8049b321447f5c0872bebf5b79
Martua Nasution <marithonst@gmail.com>
2022-04-21 02:46:56 +0700

    initial commit

--- config/config.go
+++ config/config.go
@@ -10,9 +10,9 @@ import (
 // Config func to get env value
 func Config(key string) string {
 	// load .env file
-	err := godotenv.Load(".env")
+	err := godotenv.Load(".env.development")
 	if err != nil {
-		fmt.Print("Error loading .env file")
+		fmt.Print("Error loading .env.development file")
 	}
 	// Return the value of the variable
 	return os.Getenv(key)
